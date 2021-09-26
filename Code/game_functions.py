8a3c1db620faafc7b7d11021ba3713a0a10703f1
Muhammad Mustafa <74109263+devmustafa4@users.noreply.github.com>
2021-09-26 06:23:23 +0500

    Update
    Fixed:
     - the crashing of program on start
     - no sign up and login
     - cannot get out of paint shop
    Known Bugs:
     - correct placement of car after getting out of paint shop
    Other Changings:
    - removed collision with objects while driving

---
+++ Code/game_functions.py
@@ -0,0 +1,209 @@
+import pygame
+import math
+import random
+
+particles = []
+def create_objects(grounds, num_objects, object_width, object_height, padx, pady):
+    objects = []
+
+    for ground_rect in grounds:
+        pos_x = ground_rect.x + padx
+        pos_y = ground_rect.y + pady
+
+        for create in range(num_objects):
+
+            if pos_x + object_width > ground_rect.topright[0]:
+                pos_x = ground_rect.x + padx
+                pos_y += object_height + pady
+
+            if pos_y + object_height > ground_rect.bottom:
+                break
+
+            object_rect = pygame.Rect(pos_x, pos_y, object_width, object_height)
+            objects.append(object_rect)
+            pos_x += object_width + padx
+    return objects
+
+def create_cars(grounds, pad_x, pad_y, object_width, object_height):
+    object_cars = []
+
+    for ground_rect in grounds:
+        pos_x = ground_rect.topleft[0] + pad_x
+        pos_y = ground_rect.topleft[1] + pad_y
+        object_rect = pygame.Rect(pos_x, pos_y, object_width, object_height)
+        object_cars.append(object_rect)
+
+    return object_cars
+
+def car_dicts(vertical_cars,ver_car):
+    id = 1
+    cars_list = []
+    for gari in vertical_cars:
+        car_dict = {'id': id, 'image': ver_car,'static_image':ver_car, 'rect': gari, 'prev_theeta': 0, 'theeta': 0, 'next_theeta': 90,
+                    'speed':4, 'collided_rect': None}
+        cars_list.append(car_dict)
+        id += 1
+    return cars_list
+
+def new_position(object_x,object_y,player_dict):
+
+    up , down ,left,right = player_dict['movement']
+    is_driving = player_dict['is_driving']
+    angle = player_dict['angle']
+
+    if is_driving:
+        speed = player_dict['car_speed']
+        if up:
+            object_x -= round(speed * math.cos(math.radians(angle + 90)))
+            object_y += round(speed * math.sin(math.radians(angle + 90)))
+
+        if down:
+            object_x += round(speed * math.cos(math.radians(angle + 90)))
+            object_y -= round(speed * math.sin(math.radians(angle + 90)))
+    else:
+        speed = player_dict['char_speed']
+        if player_dict['is_scrolling']:
+            up,down,left,right = player_dict['is_scrolling']
+        else:
+            up,down,left,right = player_dict['movement']
+        if up:
+            object_y += round(speed * math.sin(math.radians(angle )))
+
+        if down:
+            object_y += round(speed * math.sin(math.radians(angle)))
+
+        if right:
+            object_x -= round(speed * math.cos(math.radians(angle)))
+
+        if left:
+            object_x -= round(speed * math.cos(math.radians(angle)))
+
+
+    return object_x,object_y
+
+
+def display_particles(location, screen):
+    # [Location , velocity/direction , radius of circle]
+
+    particles.append([location, [random.randint(0, 30) / 10 - 1, -5], random.randint(2, 10)])
+    for particle in particles:
+        particle[0][0] -= random.randint(1, 3) * particle[1][0]
+        # Adding velocity about x-axis on location of x-axis to move it horizontally
+
+        particle[0][1] += random.randint(1, 4)* 0.01 * particle[1][1]
+        # Adding velocity about y-axis on y-axis location to move it.
+
+        particle[2] -= 0.2
+        # Particles get smaller over time, the circles get smaller so this will also be the radius of the circle
+        # which gets reduced over time.
+
+        pygame.draw.circle(screen, (60, 63, 65), [(int(particle[0][0])), (int(particle[0][1]))], int(particle[2]))
+        if particle[2] <= 0:
+            particles.remove(particle)
+
+def collision_side(screen,player_dict, obj_dict):
+    col_eff = 10
+    rect = player_dict['rect']
+    is_driving = player_dict['is_driving']
+    collider = [obj_dict['houses_1'],obj_dict['houses_2'],obj_dict['lane_vertical'],obj_dict['lane_horizontal']]
+    up,down ,left,right = player_dict['movement']
+    for objects in collider:
+        for obj in objects:
+            if not is_driving:
+                if rect.colliderect(obj):
+                    if abs(rect.top - obj.bottom) < col_eff:  # or abs(rect.top - tile.top) < col_eff:
+                        up =  False
+
+                    elif abs(rect.bottom - obj.top) < col_eff:  # or abs(rect.bottom - tile.bottom) < col_eff:
+                        down =  False
+
+                    elif abs(rect.left - obj.right) < col_eff:
+                        left =  False
+                    elif abs(rect.right - obj.left) < col_eff:
+                        right = False
+                player_dict['movement'] = up,down ,left,right
+            # else:
+            # # Code to handle collisions with other objects while driving
+            #     if rect.colliderect(obj):
+            #         if abs(obj.top - rect.bottom) < 20:
+            #             rect.bottom = obj.top + 1
+            #         elif abs(obj.bottom - rect.top) < 20:
+            #             rect.top = obj.bottom - 1
+            #         elif abs(obj.right - rect.left) < 20:
+            #             rect.left = obj.right
+            #         elif abs(obj.left - rect.right) < 20:
+            #             rect.right = obj.left
+
+def scroll_objects(screen,player_dict, objects_list):
+    up,down,left,right = player_dict['movement']
+
+
+    angle = player_dict['angle']
+    is_driving = player_dict['is_driving']
+    char_sprite = player_dict['animation']
+    player_rect = player_dict['rect']
+    is_scrolling = player_dict['is_scrolling']
+
+
+
+
+    for object_list in objects_list.values():
+        for object in object_list:
+            if is_scrolling:
+                keys = pygame.key.get_pressed()
+                up = keys[pygame.K_w]
+                down = keys[pygame.K_s]
+                left = keys[pygame.K_a]
+                right = keys[pygame.K_d]
+                player_dict['is_scrolling'] = [up ,down,left,right]
+
+
+            elif not is_driving and not is_scrolling:
+                if player_dict['walk'] + 1 >= player_dict['walk_limit']:
+                    player_dict['walk'] = 0
+                if up:
+                    player_dict['angle'] = 90
+                    player_dict['walk'] += 1
+                    player_dict['image'], player_dict['rect']= rot_center(char_sprite[player_dict['walk']],player_rect, angle)
+                elif down:
+                    player_dict['angle'] = 270
+                    player_dict['walk'] += 1
+                    player_dict['image'], player_dict['rect'] = rot_center(char_sprite[player_dict['walk']], player_rect,angle)
+                elif right:
+                    player_dict['angle'] = 0
+                    player_dict['walk'] += 1
+                    player_dict['image'], player_dict['rect'] = rot_center(char_sprite[player_dict['walk']], player_rect,angle)
+                elif left:
+                    player_dict['angle'] = 180
+                    player_dict['walk'] +=1
+                    player_dict['image'], player_dict['rect']= rot_center(char_sprite[player_dict['walk']], player_rect,angle)
+                else:
+                    player_dict['angle']=angle
+                    player_dict['walk'] = 0
+                    player_dict['image'],player_dict['rect']=rot_center(char_sprite[player_dict['walk']],player_rect,angle)
+            else:
+                player_dict['image'],player_dict['rect'] = rot_center(player_dict['static_car'], player_dict['rect'], angle)
+
+                if left and (up or down):
+                    angle += 0.01
+                if right and (up or down):
+                    angle -= 0.01
+                player_dict['angle'] = angle
+            object.x , object.y = new_position(object.x, object.y, player_dict)
+
+def rot_center(image, rect, angle):
+    """rotate an image while keeping its center"""
+
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = rot_image.get_rect(center=rect.center)
+
+    return rot_image, rot_rect
+
+
+def collided_obj(player_dict, obj_dict_list, req_key):
+    for obj_dict in obj_dict_list:
+        if player_dict['rect'].colliderect(obj_dict[req_key]):
+            player_dict['angle'] = obj_dict['theeta']
+            obj_dict_list.remove(obj_dict)
+            return True
+    return False
---
+++ Code/space_shooter.py
@@ -0,0 +1,100 @@
+from explosions import *
+
+# background image
+bg = pygame.image.load('Images\\bg.jpg')
+
+# initializing the player
+player = pygame.image.load('Images\\spaceship.png')
+player_rect = player.get_rect()
+player_rect.center = (400, 500)
+
+# loading asteriods
+asteriod = pygame.image.load('Images\\asteroid.png')
+asteriod_rect = asteriod.get_rect()
+asteriod_rect.x = random.randint(310,460)
+asteriod_rect.y = 50
+
+# loading bullets
+bullet = pygame.image.load('Images\\bullet.png')
+bullet_rect = bullet.get_rect()
+fire = False
+
+# font
+font = pygame.font.SysFont("Comic Sans MS", 30)
+coins_text = font.render('Coins: ', True, (250,250,250))
+
+def space_shooter(screen, coins,state, username, ended):
+    global player_rect, fire
+    screen.blit(bg, (0, 0))
+    keys = pygame.key.get_pressed()
+    if not ended:
+
+        state = 'space_shooter'
+        # gets all pressed keys
+
+        if keys[pygame.K_LEFT] and 300 < player_rect.x:
+            player_rect.x -= 5
+        if keys[pygame.K_RIGHT] and player_rect.x < 500:
+            player_rect.x += 5
+        if keys[pygame.K_SPACE] and not fire:
+            fire = True
+            bullet_rect.x = player_rect.x
+            bullet_rect.y = player_rect.y
+
+        # if asteriod gets out of the screen
+        if asteriod_rect.y > 600:
+            asteriod_rect.x = random.randint(300, 500)
+            asteriod_rect.y = -50
+
+        if fire:
+            if bullet_rect.y > 0:
+                bullet_rect.y -= 10
+                screen.blit(bullet, bullet_rect)
+            else:
+                bullet_rect.y = player_rect.y + 50
+                fire = False
+
+        # starting position of asteriod
+        asteriod_rect.y += 10
+
+        screen.blit(asteriod, asteriod_rect)
+        screen.blit(player, player_rect)
+
+        if player_rect.colliderect(asteriod_rect):
+            explode(screen, [player_rect.centerx, player_rect.centery], (255, 201, 14), 10,ended)
+            asteriod_rect.x = random.randint(310, 460)
+            asteriod_rect.y = 50
+            ended = True
+
+        if asteriod_rect.colliderect(bullet_rect):
+            asteriod_rect.y = -50
+            asteriod_rect.x = random.randint(300, 500)
+            fire = False
+            bullet_rect.y = player_rect.y + 50
+            coins += 10
+        # Displaying the coins on screen
+        coins_surface = font.render(str(coins), True, (200, 200, 200))
+        screen.blit(coins_text, (20, 20))
+        screen.blit(coins_surface, (100, 20))
+        return state,coins,ended
+    else:
+        # explosive effect
+        end_screen = explode(screen, [player_rect.centerx, player_rect.centery], (255, 201, 14), 10, ended)
+        state = 'space_shooter'
+        if end_screen:
+            state = game_over(screen, username, coins)
+
+        return state, coins, ended
+
+def game_over(screen,username, coins):
+    game_over = pygame.image.load('Images\\gameover.png')
+    screen.blit(game_over, game_over.get_rect())
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_ESCAPE]:
+        with open(f"profiles\\profile_{username}.txt", "w") as file:
+            user_profile = {'username': username, 'coins': coins}
+            file.writelines(f'{user_profile}')
+        return 'main_game'
+    else:
+        return 'space_shooter'
